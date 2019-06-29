a2302b63c2e461233c8bb4cc1e735409157322c9
cowcow <root@localhost.localdomain>
2019-06-29 23:07:26 +0900

    config読み込みを追加

--- lib/project_home.rb
+++ lib/project_home.rb
@@ -8,10 +8,13 @@ module ProjectHome
   
   HOME = Pathname($0).dirname.expand_path.ascend.find{ |path| (path+'home.yaml').exist? }.freeze
   
-  def home()
+  def self.home()
     HOME
+  end
+  
+  def self.config()
+    YAML.load_file( HOME+'home.yaml' )
   end
   
 end
 
-p ProjectHome::HOME
