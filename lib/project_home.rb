704394559d5aed095085a423184318e9050ade69
cowcow <root@localhost.localdomain>
2021-06-23 09:55:41 -0400

    auto_require,ProjectHome::HOME

--- lib/project_home.rb
+++ lib/project_home.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 require 'pathname'
 require 'yaml'
@@ -5,23 +6,34 @@ require 'yaml'
 require_relative "project_home/version"
 
 module ProjectHome
-  
-  HOME = [Dir.pwd,$0,__FILE__].lazy.map{ |file|
-    Pathname(file).dirname.expand_path.ascend.find{ |path|
-      (path+'home.yaml').exist?
-    }.freeze
-  }.select{ |path| path }.first
-  
   def self.home()
-    HOME
+    @@home ||= [Dir.pwd, $0, __FILE__].lazy.map{ |file|
+      Pathname(file).dirname.expand_path.ascend.find{ |path|
+        (path + 'home.yaml').exist?
+      }.freeze
+    }.select{ |path| path }.first
+  end
+  def self.const_missing(id)
+    if( id == :HOME )
+      const_set( id, ProjectHome.home )
+    else
+      warn "undefined constant #{id.inspect}"
+    end
+  end
+
+  def self.require( path )
+    Kernel::require (ProjectHome.home+path).to_s
   end
   
-  def self.require( path )
-    Kernel::require HOME+path
+  def self.auto_require()
+    requires = ProjectHome.config['requires'] || []
+    requires.each { |path|
+      ProjectHome.require path
+    }
   end
   
   def self.config()
-    YAML.load_file( HOME+'home.yaml' )
+    YAML.load_file( ProjectHome.home+'home.yaml' )
   end
   
 end
--- lib/project_home/version.rb
+++ lib/project_home/version.rb
@@ -1,3 +1,3 @@
 module ProjectHome
-  VERSION = "0.2.2"
+  VERSION = "0.3.0"
 end
